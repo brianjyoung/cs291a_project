0d56d3327f9dd4ed132a21913bb79fc4c7f1507e
HS88 <harmeet.2303@outlook.com>
2019-06-10 23:32:12 -0700

    testing code

--- test.py
+++ test.py
@@ -71,9 +71,12 @@ scaler.fit(training_features)
 training_normalized = scaler.transform(training_features)
 pca = PCA(0.95)
 pca.fit(training_normalized)
+
+pickle.dump(pca, open('pca.pk', 'wb'))
+
 training_transformed = pca.transform(training_normalized)
 
-pickle.dump(training_features, open('training_transformed_after_PCA.pk', 'wb'))
+pickle.dump(scaler, open('scaler.pk', 'wb'))
 
 
 # Train logistic regression
@@ -81,40 +84,45 @@ logistic_regression = LogisticRegression(solver='lbfgs')
 logistic_regression.fit(training_transformed, training_labels)
 training_predictions = logistic_regression.predict(training_transformed)
 
+pickle.dump( logistic_regression, open('logistic_regression.pk', 'wb'))
+
 
 # Read test files
 files = os.listdir('test_data')
-files = files[:100]
+
 test_labels = []
 test_features = np.empty((len(files), 60))
 for i, file in enumerate(files):
     # Get label for file
-    file_name = file
+    try:
+        file_name = file
     # Extract lip as 60x80 image
-    file = os.path.join('test_data', file)
-    fe = FeatureExtractor.from_image(file)
-    fe.face_detect()
-    fe.landmark_detect()
-    fe.crop_lips()
-    # print("File {} lips extracted.".format(i))
+        file = os.path.join('test_data', file)
+        fe = FeatureExtractor.from_image(file)
+        fe.face_detect()
+        fe.landmark_detect()
+        fe.crop_lips()
+        # print("File {} lips extracted.".format(i))
 
-    # Convert lip image to 60-dimensional feature vector
-    if fe.lips is None:
-        print("File {} skipped, lips could not be found.".format(i))
+        # Convert lip image to 60-dimensional feature vector
+        if fe.lips is None:
+            print("File {} skipped, lips could not be found.".format(i))
+            continue
+        filters = MSA.multiscale_full(fe.lips[0].flatten('F'))
+        differences = filters[1:] - filters[:-1]
+        test_features[i] = np.sum(differences, 1)
+        test_labels.append(label_dict_test[file_name])
+    except:
         continue
-    filters = MSA.multiscale_full(fe.lips[0].flatten('F'))
-    differences = filters[1:] - filters[:-1]
-    test_features[i] = np.sum(differences, 1)
-    test_labels.append(label_dict_test[file_name])
-    # print("File {} complete.".format(i))
+        # print("File {} complete.".format(i))
 
 # Crop training features, in case any faces could not be detected
-test_features = test_features[:len(training_labels), ]
+test_features = test_features[:len(test_labels), ]
 
 # Apply PCA mapping on (normalized) test data
-scaler.fit(test_features)
+
 test_normalized = scaler.transform(test_features)
-test_transformed = pca.transform(training_normalized)
+test_transformed = pca.transform(test_normalized)
 
 # Test logistic regression
 testing_predictions = logistic_regression.predict(test_transformed)
